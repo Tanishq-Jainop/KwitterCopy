beec020d44b0c0e4778cd947933c25d6bce91140
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-07-24 15:56:15 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,12 +1,13 @@
 
 
-var firebaseConfig = {
+ var firebaseConfig = {
       apiKey: "AIzaSyAlq1f-APqUVUesTNYzgu-GFqaHixdnFAQ",
       authDomain: "quiter-app.firebaseapp.com",
+      databaseURL: "https://quiter-app-default-rtdb.firebaseio.com",
       projectId: "quiter-app",
       storageBucket: "quiter-app.appspot.com",
       messagingSenderId: "959425266885",
-      appId: "1:959425266885:web:540ca484d7b4a475419cac"
+      appId: "1:959425266885:web:f788e2936712194c419cac"
     };
     
     firebase.initializeApp(firebaseConfig);
